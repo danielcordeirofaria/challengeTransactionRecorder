91e70c16d275f0b87005bbe5a3ad68495d619755
danielcordeirofaria <danielcordeirofaria@yahoo.com.br>
2024-11-05 13:33:20 -0300

    removendo arquivos

--- TransactionRecorder/TransactionRecorder/src/main/resources/db/migration/V1__Criation_of_Tables.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE TRANSACTION (
-    id_transaction INT PRIMARY KEY AUTO_INCREMENT,
-    description VARCHAR(50) NOT NULL UNIQUE,
-    date DATETIME NOT NULL,
-    puchase_amount DOUBLE NOT NULL
-);
